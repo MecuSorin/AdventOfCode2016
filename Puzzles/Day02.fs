b6fed51b9d7475ef59ad6eadc1578a8903285034
Mecu Mihai Sorin <mecusorin@gmail.com>
2016-12-03 00:11:34 +0200

    Added paket.bootstrapper.exe

--- Puzzles/Day02.fs
+++ Puzzles/Day02.fs
@@ -19,6 +19,30 @@ let moveOnKeypad key instruction =
     | Right, k when 0 = k % 3 -> k
     | Right, _ -> key + 1 
 
+let moveOnAdvancedKeyPad key instruction =
+    match instruction, key with
+    | Up, 3 -> 1
+    | Up, 13 -> 11
+    | Up, k when k>5 && k <> 9 -> k - 4
+    | Up, k -> k
+    | Down, 11 -> 13
+    | Down, 1 -> 3
+    | Down, k when k < 9 && k <>5 -> k + 4
+    | Down, k -> k
+    | Left, 1 -> 1
+    | Left, 2 -> 2
+    | Left, 5 -> 5
+    | Left, 10 -> 10
+    | Left, 13 -> 13
+    | Left, k -> k - 1
+    | Right, 1 -> 1
+    | Right, 4 -> 4
+    | Right, 9 -> 9
+    | Right, 12 -> 12
+    | Right, 13 -> 13
+    | Right, k -> k + 1
+    
+
 let getKeyPadMove letter =
     match letter with
     | 'U' -> Some Up
@@ -28,7 +52,7 @@ let getKeyPadMove letter =
     | _ -> None
 
 
-let findTheCode (input: String) (startAt: int) =
+let genericCodeFinder moveOnKeyPadStrategy (input: String) (startAt: int) =
     let listOfRows = Array.toList (input.Split ([|'\r'; '\n'|], System.StringSplitOptions.RemoveEmptyEntries))
     (startAt, listOfRows)
     |> Seq.unfold (fun (startingKey, rows) ->
@@ -39,7 +63,13 @@ let findTheCode (input: String) (startAt: int) =
                 |> Seq.map getKeyPadMove
                 |> Seq.filter Option.isSome
                 |> Seq.map Option.get
-                |> Seq.fold moveOnKeypad startingKey
+                |> Seq.fold moveOnKeyPadStrategy startingKey
                 |> fun resultedKey -> Some (resultedKey, (resultedKey, tail)))
-    |> Seq.map string
+    |> Seq.map (sprintf "%X")
     |> String.concat ""
+
+let findTheCodeSimpleKeypad input =
+    genericCodeFinder moveOnKeypad input 5
+
+let findTheCodeAdvancedKeypad input =
+    genericCodeFinder moveOnAdvancedKeyPad input 5
