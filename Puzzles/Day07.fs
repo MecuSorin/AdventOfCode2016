5db9f7822e8a846a75ebc5b2af6441cbdb59921c
Mecu Mihai Sorin <mecusorin@gmail.com>
2016-12-09 12:34:15 +0200

    Day 07 second puzzle

--- Puzzles/Day07.fs
+++ Puzzles/Day07.fs
@@ -6,5 +6,49 @@ open System.Text.RegularExpressions
 let tlsPattern = "(\w{1})(?!\1)(\w{1})\2\1"
 let tls = new Regex(tlsPattern)
 let antiTls = new Regex("\[\w*" + tlsPattern + "\w*\]")
-let isTLS input = 
+let isTLSRegex input = 
     (not (antiTls.IsMatch(input))) && (tls.IsMatch(input))
+
+// puzzle 2 without regex
+let isABAMatch (listOf3Letters: char []) = 
+    listOf3Letters.[0] <> listOf3Letters.[1] && listOf3Letters.[0] = listOf3Letters.[2]
+
+let listOfABA (text : string) = 
+    text
+    |> Seq.windowed 3
+    |> Seq.filter isABAMatch
+    |> Seq.map (String.Concat)
+
+let partitions (row: string) =
+    let comutatingFolder (odd, even) chunk = 
+        chunk :: even, odd
+    row.Split ([|'[';']'|])
+    |> Array.fold comutatingFolder ([],[])
+
+let containReflection (listOfABAItems: string list) (aba: string) =
+    let reflection =
+        String.Concat [ aba.[1]; aba.[0]; aba.[1]]
+    listOfABAItems
+    |> List.exists (fun txt -> txt.Contains(reflection))
+
+let isSSL row =
+    let hyperNet, superNet = partitions row
+    superNet
+    |> Seq.collect listOfABA
+    |> Seq.exists (containReflection hyperNet)
+
+
+// puzzle 2 regex attempt, not used anymore since is taking to long
+let abaPattern = "(\w{1})(?!\1)(\w{1})\1"
+let babPattern = "\2\1\2"
+
+let anyPattern = "(?:\w*\[\w*\]\w*)*"     // check that [ have pair ]
+let sslPattern1 = anyPattern + abaPattern + anyPattern + "\[\w*" + babPattern + "\w*\]"
+let sslPattern2 = anyPattern + "\[\w*" + abaPattern + "\w*\]" + anyPattern + babPattern
+
+let sslRegex1 = new Regex( sslPattern1)
+let sslRegex2 = new Regex ( sslPattern2)
+
+let isSSLRegex input = 
+    sslRegex1.IsMatch(input) || sslRegex2.IsMatch(input)
+
--- Specs/Day07.fs
+++ Specs/Day07.fs
@@ -3,10 +3,21 @@ module Day07
 open Puzzles.Day07
 open Expecto
 open Samples
+open System
 
-let isTLSTest input expected = 
+let isTLSRegexTest input expected = 
     test input {
-        Expect.equal (isTLS input) expected input
+        Expect.equal (isTLSRegex input) expected input
+    }
+
+let isSSLRegexTest input expected = 
+    test input {
+        Expect.equal (isSSLRegex input) expected input
+    }
+
+let isSSLTest input expected = 
+    test input {
+        Expect.equal (isSSL input) expected input
     }
 
 [<Tests>]
@@ -17,17 +28,56 @@ let puzzle1Tests =
             Expect.isFalse (tls.IsMatch("aaaa")) "aaaa"
         }
 
-        isTLSTest "abba[mnop]qrst" true
-        isTLSTest "abcd[bddb]xyyx" false
-        isTLSTest "aaaa[qwer]tyui" false 
-        isTLSTest "ioxxoj[asdfgh]zxcvbn" true 
+        isTLSRegexTest "abba[mnop]qrst" true
+        isTLSRegexTest "abcd[bddb]xyyx" false
+        isTLSRegexTest "aaaa[qwer]tyui" false 
+        isTLSRegexTest "ioxxoj[asdfgh]zxcvbn" true 
         
         test "Puzzle 1" {
              let counted = 
                 readAllLines "Day07.input.txt"
-                |> Seq.filter isTLS
+                |> Seq.filter isTLSRegex
                 |> Seq.length
              Expect.equal counted 110 "Puzzle 1"
         }
     ]
 
+[<Tests>]
+let puzzle2RegexTests =
+    testList "Day 07 Puzzle 2/regex" [
+
+        isSSLRegexTest "aba[bab]xyz" true
+        isSSLRegexTest "xyx[xyx]xyx" false
+        isSSLRegexTest "zazbz[bzb]cdb" true 
+        isSSLRegexTest "aaa[kek]eke" true 
+    ]
+
+let puzzle2RegexPuzzle = 
+    // is taking too long and have a regex bug (to few matches) => no more regex
+    test "Puzzle 2 with regex" {
+        readAllLines "Day07.input.txt"
+        |> Array.mapi (fun i v -> async { 
+            printfn "%i %s" i v
+            return isSSL v })
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Seq.filter id
+        |> Seq.length |> ignore
+    }
+
+[<Tests>]
+let puzzle2WithoutRegex =
+    testList "Day 07 Puzzle 2/no regex" [
+        isSSLTest "aba[bab]xyz" true
+        isSSLTest "xyx[xyx]xyx" false
+        isSSLTest "zazbz[bzb]cdb" true 
+        isSSLTest "aaa[kek]eke" true
+
+        test "Puzzle 2 input" {
+            let counted = 
+                readAllLines "Day07.input.txt"
+                |> Seq.filter isSSL
+                |> Seq.length
+            Expect.equal counted 242 ""
+        }
+    ]
